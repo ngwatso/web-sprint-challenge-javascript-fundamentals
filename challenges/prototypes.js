d6089baf6732285dc2fa1f89fc73e4e6381892e6
Nick Watson <ngwatson79@yahoo.com>
2020-10-09 21:40:29 -0400

    Prototypes complete

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -7,7 +7,7 @@
 */
 
 function CuboidMaker(length, width, height) {
-  this length = length;
+  this.length = length;
   this.width = width;
   this.height = height;
 }
@@ -28,7 +28,7 @@ CuboidMaker.prototype.volume = function () {
   Formula for cuboid surface area of a cube: 2 * (length * width + length * height + width * height)
 */
 
-CuboidMaker.prototype.surface = function () {
+CuboidMaker.prototype.surfaceArea = function () {
   return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
 }
 
@@ -37,8 +37,10 @@ CuboidMaker.prototype.surface = function () {
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid.   
 */
 
+const cuboid = new CuboidMaker(4, 5, 5);
+
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
 
 
